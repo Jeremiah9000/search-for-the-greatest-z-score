bc6bbffa09ec4f07a011c2634529ef2cd0ad2dc0
Jeremiah9000 <56417582+Jeremiah9000@users.noreply.github.com>
2019-11-30 18:04:53 -0500

    uploading main file

---
+++ z_score_generator.py
@@ -0,0 +1,29 @@
+import numpy as np
+import time
+
+start = time.time()
+seconds = 10
+update = False
+
+print('running...')
+try:
+    with open('greatest_z.txt') as f:
+        y = float(f.read())
+except FileNotFoundError:
+    y = 0.0
+
+while True:
+    x = np.random.randn()
+    if abs(x) > abs(y):
+        update = True
+        print(f'greater z-score found! {x}')
+        y = x
+    if time.time() >= (start + seconds):
+        end = time.time()
+        if update:
+            print(f'greatest z-score found in {end - start} seconds is z of {y}')
+            with open('greatest_z.txt', 'w+') as f:
+                f.write(str(y))
+        else:
+            print(f'no z-score found in {end - start} seconds that is greater than the previous z of {y}')
+        break
