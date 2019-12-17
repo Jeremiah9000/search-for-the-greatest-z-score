f4704a47833120d7c4c1bf62111ddb2db11b3536
Jeremiah9000 <56417582+Jeremiah9000@users.noreply.github.com>
2019-12-17 15:46:26 -0500

    added a counter to provide number of cycles. casted timed seconds into integers instead of floats

--- z_score_generator.py
+++ z_score_generator.py
@@ -12,7 +12,9 @@ try:
 except FileNotFoundError:
     y = 0.0
 
+coutner = 0
 while True:
+    coutner += 1
     x = np.random.randn()
     if abs(x) > abs(y):
         update = True
@@ -20,10 +22,11 @@ while True:
         y = x
     if time.time() >= (start + seconds):
         end = time.time()
+        print(f'{coutner} cycles')
         if update:
-            print(f'greatest z-score found in {end - start} seconds is z of {y}')
+            print(f'greatest z-score found in {int(end - start)} seconds is z of {y}')
             with open('greatest_z.txt', 'w+') as f:
                 f.write(str(y))
         else:
-            print(f'no z-score found in {end - start} seconds that is greater than the previous z of {y}')
+            print(f'no z-score found in {int(end - start)} seconds that is greater than the previous z of {y}')
         break
